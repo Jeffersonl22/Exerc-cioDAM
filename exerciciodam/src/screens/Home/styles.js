283650d8105ebf57d9559b79a79f2822136c5e0c
Jefferson Silva Lopes <84870965+Jeffersonl22@users.noreply.github.com>
2022-03-16 23:14:28 -0300

    Adicionando a pasta exportada do Expo

---
+++ exerciciodam/src/screens/Home/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Platform } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 30,
+        color: 'white',
+        textAlign: 'center',
+        
+    }
+  });
+  
